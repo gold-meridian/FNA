93ace7349e62d7304445bf8466b700b0412ae44b
Stephen T <xxlennygamerxx@gmail.com>
2024-11-21 17:30:40 -0600

    Change my mind and keep these the same...

--- src/Tomat.FAudio/Usings.cs
+++
@@ -1,8 +0,0 @@
-global using FAudio;
-
-global using static FAudio.Constants;
-
-global using wchar_t = short;
-global using FAudioSourceVoice = FAudio.FAudioVoice;
-global using FAudioSubmixVoice = FAudio.FAudioVoice;
-global using FAudioMasteringVoice = FAudio.FAudioVoice;
