52478a569e928b8d710063c677bbec09629b0d1c
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-01-28 18:18:51 -0600

    remove duplicated dllimport

--- src/FNA/Graphics/FNA3D.cs
+++ src/FNA/Graphics/FNA3D.cs
@@ -169,10 +169,6 @@ namespace Microsoft.Xna.Framework.Graphics
 			FNA3D_LogFunc error
 		);
 
-		// tModLoader: Define FNA3D_LinkedVersion import.
-		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-		public static extern uint FNA3D_LinkedVersion();
-
 		#endregion
 	}
 
